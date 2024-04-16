d2b6b1393acb654f6821d514a7142d8b0e5d8bf7
Alan Hernandez <alan23bh@gmail.com>
2024-04-15 21:05:33 -0400

    Added display of word and guessed letters

--- js/script.js
+++ js/script.js
@@ -23,13 +23,13 @@ guessButton.addEventListener("click", function (e) {
   e.preventDefault();
   message.innerText = "";
   const guess = letterInput.value;
-  console.log(guess);
-  letterInput.value = "";
+  //console.log(guess);
 
   const goodGuess = checkInputValue(guess);
   if (goodGuess) {
     makeGuess(guess);
   }
+  letterInput.value = "";
 });
 const checkInputValue = function (input) {
   const acceptedLetter = /[a-zA-Z]/;
@@ -49,6 +49,38 @@ const makeGuess = function (guess) {
     message.innerText = "C'mon now you already done guessed that letter.";
   } else {
     guessedLetters.push(guess);
-    console.log(guessedLetters);
+    showGuessedLetters();
+    //console.log(guessedLetters);
+    updateWordInProgress(guessedLetters);
+  }
+};
+const showGuessedLetters = function () {
+  guessedLettersElement.innerHTML = "";
+  for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLettersElement.append(li);
+  }
+};
+const updateWordInProgress = function (guessedLetters) {
+  const wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  //console.log(wordArray);
+  const revealWord = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      revealWord.push(letter.toUpperCase());
+    } else {
+      revealWord.push("●");
+    }
+  }
+  wordInProgress.innerText = revealWord.join("");
+  checkIfWin();
+};
+const checkIfWin = function () {
+  if (word.toUpperCase() === wordInProgress.innerText) {
+    message.classList.add("win");
+    message.innerHTML = `
+      <p class="highlight">You guessed the correct word! Congrats!</p>`;
   }
 };
